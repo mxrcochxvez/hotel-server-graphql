b51cfeaa19af05c8572eb5d1ed25691e875717c2
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-08-26 13:53:16 -0700

    feat(drizzle): sets up the drizzle client with sqlite

---
+++ src/modules/db/client.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+import { createClient } from '@libsql/client/.';
+import { drizzle } from 'drizzle-orm/libsql';
+
+const client = createClient({ url: process.env.DB_FILE_NAME! });
+export const db = drizzle({ client });
