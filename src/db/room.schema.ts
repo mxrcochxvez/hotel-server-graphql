c0a5f87f4572b57d4093dc50e850e049f10ff381
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-08-27 10:07:47 -0700

    fix(room.schema): removes un-needed columna and adds room type column

--- src/db/room.schema.ts
+++ src/db/room.schema.ts
@@ -1,6 +1,6 @@
-import { int, sqliteTable } from "drizzle-orm/sqlite-core";
+import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const roomsTable = sqliteTable("rooms", {
 	number: int().primaryKey(),
-	isMaster: int().default(0),
+	type: text().notNull(),
 });
