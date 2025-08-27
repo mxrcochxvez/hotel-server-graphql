2d0da642c2e1a5f1e84d42d322263604685b3015
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-08-26 23:16:35 -0700

    fix(tsconfig.json): resolves runtime errors from missing types in libraries

--- src/repos/db/client.ts
+++ src/repos/db/client.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { createClient } from '@libsql/client/.';
 import { drizzle } from 'drizzle-orm/libsql';
+import * as schema from '../../db/index';
 
 const client = createClient({ url: process.env.DB_FILE_NAME! });
-export const db = drizzle({ client });
+export const db = drizzle(client, { schema });
