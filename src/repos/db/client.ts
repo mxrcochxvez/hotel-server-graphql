ca281f43af6dfc42e6056daf3f5c3180838754f6
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-08-26 23:30:32 -0700

    docs(client): introduces db type so we can require it across the app

--- src/repos/db/client.ts
+++ src/repos/db/client.ts
@@ -1,7 +1,11 @@
 import 'dotenv/config';
-import { createClient } from '@libsql/client/.';
-import { drizzle } from 'drizzle-orm/libsql';
+import { Client, createClient } from '@libsql/client/.';
+import { drizzle, LibSQLDatabase } from 'drizzle-orm/libsql';
 import * as schema from '../../db/index';
 
+export type DBType = LibSQLDatabase<typeof schema> & {
+	$client: Client
+};
+
 const client = createClient({ url: process.env.DB_FILE_NAME! });
-export const db = drizzle(client, { schema });
+export const db: DBType = drizzle(client, { schema });
