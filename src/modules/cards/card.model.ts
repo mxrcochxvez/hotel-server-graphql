6567fd842a1da5113945a935ebfc924e991e92fc
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-08-26 23:52:08 -0700

    feat(card): uses the cards model to get a card by ID

--- src/modules/cards/card.model.ts
+++ src/modules/cards/card.model.ts
@@ -1,14 +1,18 @@
-import { db } from '../../repos/db/client';
+import { eq } from 'drizzle-orm';
+import { db, DBType } from '../../repos/db/client.js';
+import { cardsTable } from '../../db/index.js';
 
 class CardModel {
-	#db: any;
+	#db: DBType;
 
-	constructor(drizzleClient: any) {
+	constructor(drizzleClient: DBType) {
 		this.#db = drizzleClient;
 	}
 
 	find(id: string) {
-		return this.#db.query
+		return this.#db.query.cardsTable.findFirst({
+			where: eq(cardsTable.id, id),
+		});
 	}
 }
 
--- src/modules/cards/card.resolvers.ts
+++ src/modules/cards/card.resolvers.ts
@@ -1,7 +1,15 @@
+import { cardModel } from "./card.model.js";
+
 export default {
 	Query: {
 		test() {
 			return 'test';
+		},
+
+		getCard(args) {
+			console.log(args);
+
+			return cardModel.find('test');
 		}
 	}
 }
