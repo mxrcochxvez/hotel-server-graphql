c61e065670776b73f0d6393ebb8fcca0f3b23aff
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-09-01 15:19:41 -0700

    feat(reservation.model): checks to see if a reservation is active

--- src/modules/reservations/reservation.model.ts
+++ src/modules/reservations/reservation.model.ts
@@ -1,6 +1,8 @@
+import { and, eq, lte, gt } from "drizzle-orm";
 import { reservationsTable } from "../../db/index.js";
-import { DBType } from "../../repos/db/client";
-import { BaseModel } from "../model/base.model";
+import { DBType } from "../../repos/db/client.js";
+import { AtLeastOne } from "../../utilities/atleast-one.js";
+import { BaseModel } from "../model/base.model.js";
 
 type Reservation = {
 	roomNumber: number,
@@ -9,6 +11,8 @@ type Reservation = {
 	guestId: string,
 	cardNumber: string,
 }
+
+type SearchableReservationData = AtLeastOne<Reservation>;
 
 type ReservationInput =
 	Omit<Reservation, 'checkinDate' | 'checkoutDate'> & {
@@ -30,4 +34,42 @@ class ReservationModel extends BaseModel<'reservationsTable', typeof reservation
 			checkoutDate: isoDate(reservation.checkoutDate),
 		});
 	}
+
+	isReservationActive({
+		roomNumber,
+		checkinDate,
+		checkoutDate,
+		guestId,
+		cardNumber,
+	}: SearchableReservationData) {
+		const nowIso = new Date().toISOString();
+
+		const clauses: any[] = [];
+
+		if (roomNumber !== undefined) {
+			clauses.push(eq(reservationsTable.roomNumber, roomNumber));
+		}
+		if (guestId !== undefined) {
+			clauses.push(eq(reservationsTable.guestId, guestId));
+		}
+		if (cardNumber !== undefined) {
+			clauses.push(eq(reservationsTable.cardNumber, cardNumber));
+		}
+		if (checkinDate !== undefined) {
+			const checkin = new Date(checkinDate).toISOString();
+
+			clauses.push(eq(reservationsTable.checkinDate, checkin));
+		}
+		if (checkoutDate !== undefined) {
+			const checkout = new Date(checkoutDate).toISOString();
+
+			clauses.push(eq(reservationsTable.checkoutDate, checkout));
+		}
+
+		clauses.push(lte(reservationsTable.checkinDate, nowIso));
+		clauses.push(gt(reservationsTable.checkoutDate, nowIso));
+
+		return this.read({ where: and(...clauses) });
+	}
+
 }
