f9e000be98e86ad57b79a9bd0f882a42001da432
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-09-01 15:43:50 -0700

    revert(client.ts): reverts to initial client initialization

--- src/repos/db/client.ts
+++ src/repos/db/client.ts
@@ -1,15 +1,11 @@
 import 'dotenv/config';
-import { Client } from '@libsql/client';
+import { Client, createClient } from '@libsql/client';
 import { drizzle, LibSQLDatabase } from 'drizzle-orm/libsql';
-import * as schema from '../../db/index.js';
+import * as schema from '../../db';
 
 export type DBType = LibSQLDatabase<typeof schema> & {
 	$client: Client
 };
 
-export const db = drizzle({
-	connection: {
-		url: process.env.DB_FILE_NAME,
-	},
-	schema
-});
+const client = createClient({ url: process.env.DB_FILE_NAME! });
+export const db: DBType = drizzle(client, { schema });
