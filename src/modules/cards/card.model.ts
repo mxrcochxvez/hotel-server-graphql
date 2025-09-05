03b47d0ad92889757aeef2d97eeff433c6bb8b7e
Marco Chavez <marcode.chavez.jr@gmail.com>
2025-09-05 08:39:15 -0700

    feat(card.model): allows you to bulk create cards

--- src/modules/cards/card.model.ts
+++ src/modules/cards/card.model.ts
@@ -1,4 +1,4 @@
-import { eq } from 'drizzle-orm';
+import { eq, isNull, SQL } from 'drizzle-orm';
 import { db, DBType } from '../../repos/db/client.js';
 import { cardsTable } from '../../db/index.js';
 
@@ -13,6 +13,28 @@ class CardModel {
 		return this.#db.query.cardsTable.findFirst({
 			where: eq(cardsTable.id, id),
 		});
+	}
+
+	bulkCreate(amount: number, startingPoint: number = 1) {
+		const iterateOver = Array.from({ length: amount });
+
+		const createPromises = iterateOver.map((_, index) => {
+			const cardNumber = index + startingPoint;
+
+			return this.#db.insert(cardsTable).values({ id: `${cardNumber}` });
+		});
+
+		return Promise.all(createPromises);
+	}
+
+	listCards(where?: SQL<unknown>) {
+		return this.#db.query.cardsTable.findMany({
+			where,
+		});
+	}
+
+	listAvailableCards() {
+		return this.listCards(isNull(cardsTable.roomNumber));
 	}
 }
 
--- src/modules/cards/card.resolvers.ts
+++ src/modules/cards/card.resolvers.ts
@@ -1,15 +1,24 @@
+import { respond } from "../../utilities/graphql-response.js";
 import { cardModel } from "./card.model.js";
 
 export default {
 	Query: {
-		test() {
-			return 'test';
-		},
-
-		getCard(args) {
-			console.log(args);
-
-			return cardModel.find('test');
+		listAvailableCards() {
+			return listAvailableCards();
 		}
-	}
+	},
+
+	Mutation: {
+		bulkCreate() {
+			return bulkCreate();
+		},
+	},
+}
+
+function listAvailableCards() {
+	return respond(() => cardModel.listAvailableCards());
+}
+
+function bulkCreate() {
+	return respond(() => cardModel.bulkCreate(10, 0));
 }
